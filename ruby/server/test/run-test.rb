76c74a7319ce32503d54be53b64e714f82bdfff4
junji torii <jun930@gmail.com>
2010-02-12 15:26:49 +0900

    bugfix:LOAD_PATH of client was corrected.

--- ruby/server/test/run-test.rb
+++ ruby/server/test/run-test.rb
@@ -9,7 +9,7 @@ $LOAD_PATH.unshift("#{base_path}/commons/lib")
 $LOAD_PATH.unshift("#{base_path}/server/test")
 
 client_base_path = Pathname(__FILE__).dirname.parent.parent.parent.parent.expand_path
-$LOAD_PATH.unshift("#{client_base_path}/roma-ruby-client/ruby/client/lib")
+$LOAD_PATH.unshift("#{client_base_path}/roma-ruby-client/lib")
 
 require 'roma-test-utils'
 
