7fbd54d8b79bfbd26170763ef3becda627f2a297
junji torii <jun930@gmail.com>
2010-04-04 16:12:35 +0900

    added a get_key_info(key) method

--- ruby/server/lib/roma/plugin/plugin_debug.rb
+++ ruby/server/lib/roma/plugin/plugin_debug.rb
@@ -14,6 +14,18 @@ module Roma
         send_data("#{e}\r\n")
       end
 
+      def get_key_info(key)
+        d = Digest::SHA1.hexdigest(key).hex % @rttable.hbits
+        vn = @rttable.get_vnode_id(d)
+        nodes = @rttable.search_nodes_for_write(vn)
+        s = sprintf("d = %s 0x%x\r\n",d,d)
+        send_data(s)
+        s = sprintf("vn = %s 0x%x\r\n",vn,vn)
+        send_data(s)
+        send_data("nodes = #{nodes.inspect}\r\n")
+        "END"
+      end
+
     end
   end
 end
