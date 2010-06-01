6ad13aabee6e6acb48f3bf7265a4436dc6c4a761
junji torii <jun930@gmail.com>
2010-06-01 20:29:30 +0900

    added a memory free process in test methods

--- c/main.c
+++ c/main.c
@@ -18,19 +18,22 @@ void test_serialize() {
     valinfo1.length = 3599;    
     rmc_set("test-s-key-0001", valinfo1, 0);
     
-    rmc_get("test-s-key-0001");
+    valinfo1 = rmc_get("test-s-key-0001");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 }
 
 void test_simple() {
     rmc_value_info valinfo1;
     valinfo1 = rmc_get("e-empty-test-cp-key-0001");
+    if(valinfo1.value != NULL) free(valinfo1.value);
+
     //printf("[%s]\n", valinfo1.value);
 
-    valinfo1.length = 56;
     valinfo1.value = "test-cp-value-0001-test-cp-value-0001-test-cp-value-0001";
-
+    valinfo1.length = strlen(valinfo1.value);
     rmc_set("test-cp-key-0001", valinfo1, 0);
-    rmc_get("test-cp-key-0001");
+    valinfo1 = rmc_get("test-cp-key-0001");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 }
 
 void test_complex() {
@@ -38,39 +41,45 @@ void test_complex() {
     // set => get
     rmc_value_info valinfo1;
     valinfo1 = rmc_get("e-empty-test-cp-key-0001");
+    if(valinfo1.value != NULL) free(valinfo1.value);
     //printf("[%s]\n", valinfo1.value);
 
-    valinfo1.length = 56;
     valinfo1.value = "test-cp-value-0001-test-cp-value-0001-test-cp-value-0001";
+    valinfo1.length = strlen(valinfo1.value);
 
     rmc_set("test-cp-key-0001", valinfo1, 0);
-    rmc_get("test-cp-key-0001");
+    valinfo1 = rmc_get("test-cp-key-0001");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // delete => add => get
-    valinfo1.length = 56;
     valinfo1.value = "test-cp-value-0001-test-cp-value-0001-test-cp-value-0001";
+    valinfo1.length = strlen(valinfo1.value);
 
     rmc_delete("test-cp-key-0002");
     rmc_add("test-cp-key-0002", valinfo1, 0);
-    rmc_get("test-cp-key-0002");
+    valinfo1 = rmc_get("test-cp-key-0002");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // replace => get
-    valinfo1.length = 56;
-    valinfo1.value = "test-cp-value-00a1-test-cp-value-00a1-test-cp-value-00a1";
+    valinfo1.value = "test-cp-value-0001-test-cp-value-0001-test-cp-value-0001";
+    valinfo1.length = strlen(valinfo1.value);
     rmc_replace("test-cp-key-0002", valinfo1, 0);
-    rmc_get("test-cp-key-0002");
+    valinfo1 = rmc_get("test-cp-key-0002");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // append => get
-    valinfo1.length = 56;
     valinfo1.value = "-test-cp-value-0aa1-test-cp-value-0aa1-test-cp-value-0aa1";
+    valinfo1.length = strlen(valinfo1.value);
     rmc_append("test-cp-key-0002", valinfo1, 0);
-    rmc_get("test-cp-key-0002");
+    valinfo1 = rmc_get("test-cp-key-0002");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // prepend => get
-    valinfo1.length = 56;
     valinfo1.value = "test-cp-value-paa1-test-cp-value-paa1-test-cp-value-paa1-";
+    valinfo1.length = strlen(valinfo1.value);
     rmc_prepend("test-cp-key-0002", valinfo1, 0);
-    rmc_get("test-cp-key-0002");
+    valinfo1 = rmc_get("test-cp-key-0002");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // =============================
     // listcommand-1: insert => join
@@ -90,7 +99,8 @@ void test_complex() {
     valinfo5.value = "list-0000003";
     rmc_alist_insert("test-cp-key-0021", 2, valinfo5);
 
-    rmc_alist_join("test-cp-key-0021", "\t");
+    valinfo1 = rmc_alist_join("test-cp-key-0021", "\t");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // listcommand-2: include
     valinfo5.length = 12;
@@ -139,15 +149,19 @@ void test_complex() {
     valinfo5.value = "list-s000010";
     rmc_alist_sized_insert("test-cp-key-0022", 10, valinfo5);
 
-    rmc_alist_join("test-cp-key-0022", "\t");
+    valinfo1 = rmc_alist_join("test-cp-key-0022", "\t");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     valinfo5.value = "list-s000011";
     rmc_alist_sized_insert("test-cp-key-0022", 10, valinfo5);
 
-    rmc_alist_join("test-cp-key-0022", "\t");
+    valinfo1 = rmc_alist_join("test-cp-key-0022", "\t");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
-    rmc_alist_first("test-cp-key-0022");
-    rmc_alist_last("test-cp-key-0022");
+    valinfo1 = rmc_alist_first("test-cp-key-0022");
+    if(valinfo1.value != NULL) free(valinfo1.value);
+    valinfo1 = rmc_alist_last("test-cp-key-0022");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
     // listcommand-5: pop/push/shift => join
     rmc_delete("test-cp-key-0023");
@@ -161,15 +175,20 @@ void test_complex() {
     valinfo5.value = "list-p000003";
     rmc_alist_push("test-cp-key-0023", valinfo5);
 
-    rmc_alist_pop("test-cp-key-0023");
-    rmc_alist_join("test-cp-key-0023", "\t");
+    valinfo1 = rmc_alist_pop("test-cp-key-0023");
+    if(valinfo1.value != NULL) free(valinfo1.value);
+    valinfo1 = rmc_alist_join("test-cp-key-0023", "\t");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
-    rmc_alist_shift("test-cp-key-0023");
-    rmc_alist_join("test-cp-key-0023", "\t");
+    valinfo1 = rmc_alist_shift("test-cp-key-0023");
+    if(valinfo1.value != NULL) free(valinfo1.value);
+    valinfo1 = rmc_alist_join("test-cp-key-0023", "\t");
+    if(valinfo1.value != NULL) free(valinfo1.value);
 
-    rmc_alist_to_json("test-cp-key-0022");
-    rmc_alist_tostr("test-cp-key-0022");
-
+    valinfo1 = rmc_alist_to_json("test-cp-key-0022");
+    if(valinfo1.value != NULL) free(valinfo1.value);
+    valinfo1 = rmc_alist_tostr("test-cp-key-0022");
+    if(valinfo1.value != NULL) free(valinfo1.value);
  }
 
 int main(int argc, char **argv)
@@ -190,10 +209,14 @@ int main(int argc, char **argv)
 
     int ret = rmc_connect(hosts, str_hosts);
     if (ret == EXIT_FAILURE) return (EXIT_FAILURE);
-    
-    test_simple();
-    test_complex();
-    //test_serialize();
+    int cnt = 0;
+    while(1){
+      printf("cnt = %d\n",cnt++);
+      test_simple();
+      test_complex();
+      test_serialize();
+    }
     rmc_disconnect();
+
     return (EXIT_SUCCESS);
 }
