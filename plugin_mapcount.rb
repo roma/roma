4cac92107d47d6ba01d190d4ea53170b43d81a2d
yukio.goto <yukio.goto@mail.rakuten.com>
2011-12-14 17:21:24 +0900

    implement test code for mapcount_set

--- plugin_mapcount.rb
+++ plugin_mapcount.rb
@@ -7,35 +7,40 @@ module Roma
       include ::Roma::CommandPlugin
       include ::Roma::Command::Definition
 
-      def_write_command_with_key_value :mapcount_set, 5 do |ctx|
-      #def_write_command_with_key :mapcount_set do |ctx|
+      # mapcount_set <key> <flags> <expt> <sub_keys>\r\n
+      # TODO: comment for return value
+      def_write_command_with_key :mapcount_set do |ctx|
         v = {}
         v = Marshal.load(ctx.stored.value) if ctx.stored
 
-        args = ctx.argv[2].split(/\s*,\s*/)
+        args = ctx.argv[4].split(/\s*,\s*/)
         args.each do |arg|
-=begin
-          if v.key? arg
-            v[arg] += 1
+          if arg =~ /^([A-Za-z0-9]+)(:(\-?[\d]))?$/
+            key = $1
+            count = 1
+            count = $3.to_i if $3
+            v[key] ||= 0
+            v[key] += count
           else
-            v[arg] = 0
+            raise ClientErrorException, "invalid sub_keys format: #{ctx.argv[4]}"
           end
-=end
-            v[arg] = ctx.params.value
-            #v[arg] = "111"
         end
 
-        expt = ctx.argv[4].to_i
+        expt = calc_expt(ctx.argv[3].to_i)
+
+        [0, expt, Marshal.dump(v), :write, 'STORED']
+      end
+
+      private
+      def calc_expt(expt)
         if expt == 0
           expt = 0x7fffffff
         elsif expt < 2592000
           expt += Time.now.to_i
         end
 
-        #[0, expt, Marshal.dump(v), :write, 'STORED']
-        [0, ctx.stored.expt, Marshal.dump(v), :write, 'STORED']
+        expt
       end
-
     end # PluginCount
   end # CommandPlugin
 end # Roma
