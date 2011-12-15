d12ba8badc39ff8ee05b7fe8cfa5b4c638d10e38
yukio.goto <yukio.goto@mail.rakuten.com>
2011-12-15 18:23:08 +0900

    change protcol oneline to multiline and implement countup

--- plugin_mapcount.rb
+++ plugin_mapcount.rb
@@ -7,13 +7,15 @@ module Roma
       include ::Roma::CommandPlugin
       include ::Roma::Command::Definition
 
-      # mapcount_countup <key> <flags> <expt> <sub_keys>\r\n
+      # mapcount_countup <key> <expt> <sub_keys_length>\r\n
+      # <sub_keys> \r\n
+      #
       # TODO: comment for return value
-      def_write_command_with_key :mapcount_countup do |ctx|
+      def_write_command_with_key_value :mapcount_countup, 3, :multi_line do |ctx|
         v = {}
         v = Marshal.load(ctx.stored.value) if ctx.stored
 
-        args = ctx.argv[4].split(/\s*,\s*/)
+        args = ctx.params.value.split(/\s*,\s*/)
         args.each do |arg|
           if arg =~ /^([A-Za-z0-9]+)(:(\-?[\d]))?$/
             key = $1
@@ -27,21 +29,46 @@ module Roma
         end
 
         v[:last_updated_date] = Time.now.gmtime.strftime(DATE_FORMAT)
+        expt = calc_expt(ctx.argv[2].to_i)
 
-        expt = calc_expt(ctx.argv[3].to_i)
-
-        @log.info("v:#{v.inspect}")
-
-        [0, expt, Marshal.dump(v), :write, return_str(v)]
+        ret_str = return_str(v)
+        ret_msg = "VALUE #{ctx.params.key} 0 #{ret_str.length}\r\n#{ret_str}\r\nEND"
+        [0, expt, Marshal.dump(v), :write, ret_msg]
       end
 
-      # mapcount_get <key> <sub_keys>\r\n
+      # mapcount_update <key> <expt> <sub_keys_length>\r\n
+      # <sub_keys>\r\n
+      #
+      # TODO: handle sub_keys
       # TODO: comment for return value
-      def_write_command_with_key :mapcount_get do |ctx|
-        next send_data("NOT_FOUND\r\n") unless ctx.stored
+      def_write_command_with_key :mapcount_update, :multi_line do |ctx|
+        v = {}
+        v = Marshal.load(ctx.stored.value) if ctx.stored
 
-        ret = Marshal.load(ctx.stored.value) if ctx.stored
-        send_data("#{return_str(ret)}\r\n")
+        v[:last_updated_date] = Time.now.gmtime.strftime(DATE_FORMAT)
+        expt = calc_expt(ctx.argv[2].to_i)
+
+        ret_str = return_str(v)
+        ret_msg = "VALUE #{ctx.params.key} 0 #{ret_str.length}\r\n#{ret_str}\r\nEND"
+        [0, expt, Marshal.dump(v), :write, ret_msg]
+      end
+
+      # mapcount_get <key> 0 <sub_keys_str_len>\r\n
+      # <sub_keys>\r\n
+      #
+      # TODO: handle sub_keys
+      # TODO: comment for return value
+      def_read_command_with_key :mapcount_get, :multi_line do |ctx|
+        ret = nil
+        if ctx.stored
+          ret_val = Marshal.load(ctx.stored.value)
+          if ret_val.is_a?(Hash)
+            ret = return_str(ret_val)
+          end
+        end
+
+        send_data("VALUE #{ctx.params.key} 0 #{ret.length}\r\n#{ret}\r\n") if ret
+        send_data("END\r\n")
       end
 
       private
