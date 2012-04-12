2987536c5e1e1b0f83d0a8cd1736ee8ac26fdfd4
junji torii <jun930@gmail.com>
2012-04-13 01:28:23 +0900

    fix:[can't add a new key into hash during iteration] occurring in leter then ruby 1.9.1 fixed.

--- ruby/server/lib/roma/storage/rh_storage.rb
+++ ruby/server/lib/roma/storage/rh_storage.rb
@@ -41,6 +41,62 @@ module Roma
             }
           }
         end
+
+        def each_vn_dump(target_vn)
+          count = 0
+          @divnum.times{|i|
+            tn =  Time.now.to_i
+            keys = @hdb[i].keys
+            keys.each{|k|
+              v = @hdb[i][k]
+              vn, last, clk, expt, val = unpack_data(v)
+              if vn != target_vn || (expt != 0 && tn > expt)
+                count += 1              
+                sleep @each_vn_dump_sleep if count % @each_vn_dump_sleep_count == 0
+                next
+              end
+              if val
+                yield [vn, last, clk, expt, k.length, k, val.length, val].pack("NNNNNa#{k.length}Na#{val.length}")
+              else
+                yield [vn, last, clk, expt, k.length, k, 0].pack("NNNNNa#{k.length}N")
+              end
+            }
+          }
+        end
+
+        def each_hdb_dump(i,except_vnh = nil)
+          count = 0
+          keys = @hdb[i].keys
+          keys.each{|k|
+            v = @hdb[i][k]
+            vn, last, clk, expt, val = unpack_data(v)
+            if except_vnh && except_vnh.key?(vn) || Time.now.to_i > expt
+              count += 1
+              sleep @each_vn_dump_sleep if count % @each_vn_dump_sleep_count == 0
+            else
+              yield [vn, last, clk, expt, k.length, k, val.length, val].pack("NNNNNa#{k.length}Na#{val.length}")
+              sleep @each_vn_dump_sleep
+            end
+          }
+        end
+
+        # Create vnode dump.
+        def get_vnode_hash(vn)
+          buf = {}
+          count = 0
+          hdb = @hdb[@hdiv[vn]]
+          keys = hdb.keys
+          keys.each{ |k|
+            v = hdb[k]
+            count += 1
+            sleep @each_vn_dump_sleep if count % @each_vn_dump_sleep_count == 0
+            dat = unpack_data(v) #v.unpack('NNNN')
+            buf[k] = v if dat[0] == vn
+          }
+          return buf
+        end
+        private :get_vnode_hash
+        
       end
 
       private
