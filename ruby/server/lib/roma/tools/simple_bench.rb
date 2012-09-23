debb5f6142685e47a87285c97460c8811eaffa49
junji torii <jun930@gmail.com>
2012-09-23 18:19:02 +0900

    refactor:Add new functions.

--- ruby/server/lib/roma/tools/simple_bench.rb
+++ ruby/server/lib/roma/tools/simple_bench.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
+require 'optparse'
 require 'date'
 require 'roma/client/rclient'
 
-@@cnt=0
-@@tmax=0
-@@tmin=100
+@@cnt = 0
+@@tmax = 0
+@@tmin = 100
 
-Thread.new {
+Thread.new do
   sleep_time=10
   while(true)
     sleep sleep_time
@@ -15,13 +16,13 @@ Thread.new {
     @@tmax=0
     @@tmin=100
   end
-}
+end
 
-def random_rquest_sender(ini_nodes)
+def random_rquest_sender(ini_nodes, n)
+  puts __method__
   rc=Roma::Client::RomaClient.new(ini_nodes)
 
-  n=10000
-  loop{
+  loop do
     i=rand(n)
     ts = DateTime.now
     case rand(3)
@@ -39,21 +40,69 @@ def random_rquest_sender(ini_nodes)
     @@tmax=t if t > @@tmax
     @@tmin=t if t < @@tmin
     @@cnt+=1
-  }
+  end
 end
  
+def set_sequence(ini_nodes, n)
+  puts __method__
+  rc=Roma::Client::RomaClient.new(ini_nodes)
+
+  n.times do |i|
+    ts = DateTime.now
+    res=rc.set("key_#{i}","value_#{i}")
+    puts "set k=#{i} #{res}" if res==nil || res.chomp != 'STORED'
+    t=(DateTime.now - ts).to_f * 86400.0
+    @@tmax=t if t > @@tmax
+    @@tmin=t if t < @@tmin
+    @@cnt+=1
+  end
+end
+
+def get_sequence(ini_nodes, n)
+  puts __method__
+  rc=Roma::Client::RomaClient.new(ini_nodes)
+
+  n.times do |i|
+    ts = DateTime.now
+    res=rc.get("key_#{i}")
+    puts "get #{i} #{res}" if res != "value_#{i}"
+    t=(DateTime.now - ts).to_f * 86400.0
+    @@tmax=t if t > @@tmax
+    @@tmin=t if t < @@tmin
+    @@cnt+=1
+  end
+end
+
+param = { :num=>10000, :th=>1 }
+
+opts = OptionParser.new
+opts.banner = "usage:#{File.basename($0)} [options] addr:port"
+opts.on("-s", "--set", "set request"){|v| param[:set] = v }
+opts.on("-g", "--get", "get request"){|v| param[:get] = v }
+opts.on("-r", "--rand", "random request"){|v| param[:rand] = v }
+opts.on("-n", "--num [num]", "number of kyes"){|v| param[:num] = v.to_i }
+opts.on("-t", "--threads [num]", "number of threads"){|v| param[:th] = v.to_i }
+opts.parse!(ARGV)
+
 if ARGV.length == 0
-  STDERR.puts "usage:simple_bench addr:port"
+  STDERR.puts opts.help
   exit
 end
 
-tn=10
-t=[]
-tn.times{
-  t << Thread.new{
-    random_rquest_sender(ARGV)
-  }
-}
+if param.key?(:get) == false && param.key?(:set) == false
+  param[:rand] = true
+end
 
-t[0].join
+t = []
+param[:th].times do |i|
+  puts "Start thread #{i}"
+  t << Thread.new do
+    get_sequence(ARGV, param[:num]) if param.key?(:get)
+    set_sequence(ARGV, param[:num]) if param.key?(:set)
+    random_rquest_sender(ARGV, param[:num]) if param.key?(:rand)
+  end
+end
 
+t.each{|th| th.join }
+
+puts "#{File.basename($0)} has done."
