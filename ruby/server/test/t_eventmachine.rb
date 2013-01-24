50d0a84caad584841042e47b3bbdbaf21426b42a
Hiroki Matsue <hiroki.matsue@gmail.com>
2013-01-24 17:54:30 +0900

    Add test for context switch with eventmachine.

---
+++ ruby/server/test/t_eventmachine.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'eventmachine'
+
+class EventMachineTest < Test::Unit::TestCase
+  LOOP_NUM = 5
+
+  def test_context_switch
+    EM.epoll
+    t = Thread.new do
+      LOOP_NUM.times do |i|
+        100.times{ Thread.pass }
+        sleep 1
+        #puts i
+      end
+      EM.stop_event_loop
+    end
+    s = Time.now
+    EventMachine::run
+    e = Time.now
+    assert((e - s) < (LOOP_NUM + 1))
+  end
+end
