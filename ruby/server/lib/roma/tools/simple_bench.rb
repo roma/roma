9df1a5c93147de712f52c70d22c826f6b8114d03
junji torii <jun930@gmail.com>
2014-03-07 12:25:58 +0900

    fix : warning of class variable access from toplevel

--- ruby/server/lib/roma/tools/simple_bench.rb
+++ ruby/server/lib/roma/tools/simple_bench.rb
@@ -3,18 +3,18 @@ require 'optparse'
 require 'date'
 require 'roma/client/rclient'
 
-@@cnt = 0
-@@tmax = 0
-@@tmin = 100
+@cnt = 0
+@tmax = 0
+@tmin = 100
 
 Thread.new do
   sleep_time=10
   while(true)
     sleep sleep_time
-    printf("qps=%d max=%f min=%f ave=%f\n",@@cnt/sleep_time,@@tmax,@@tmin,sleep_time/@@cnt.to_f)
-    @@cnt=0
-    @@tmax=0
-    @@tmin=100
+    printf("qps=%d max=%f min=%f ave=%f\n",@cnt/sleep_time,@tmax,@tmin,sleep_time/@cnt.to_f)
+    @cnt=0
+    @tmax=0
+    @tmin=100
   end
 end
 
@@ -37,9 +37,9 @@ def random_request_sender(ini_nodes, n)
       puts "del k=#{i} #{res}" if res != 'DELETED' && res != 'NOT_FOUND'
     end
     t=(DateTime.now - ts).to_f * 86400.0
-    @@tmax=t if t > @@tmax
-    @@tmin=t if t < @@tmin
-    @@cnt+=1
+    @tmax=t if t > @tmax
+    @tmin=t if t < @tmin
+    @cnt+=1
   end
 end
  
@@ -52,9 +52,9 @@ def set_sequence(ini_nodes, n)
     res=rc.set("key_#{i}","value_#{i}")
     puts "set k=#{i} #{res}" if res==nil || res.chomp != 'STORED'
     t=(DateTime.now - ts).to_f * 86400.0
-    @@tmax=t if t > @@tmax
-    @@tmin=t if t < @@tmin
-    @@cnt+=1
+    @tmax=t if t > @tmax
+    @tmin=t if t < @tmin
+    @cnt+=1
   end
 end
 
@@ -67,9 +67,9 @@ def get_sequence(ini_nodes, n)
     res=rc.get("key_#{i}")
     puts "get #{i} #{res}" if res != "value_#{i}"
     t=(DateTime.now - ts).to_f * 86400.0
-    @@tmax=t if t > @@tmax
-    @@tmin=t if t < @@tmin
-    @@cnt+=1
+    @tmax=t if t > @tmax
+    @tmin=t if t < @tmin
+    @cnt+=1
   end
 end
 
