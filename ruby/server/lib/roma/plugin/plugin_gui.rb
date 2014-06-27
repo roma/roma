9fe40efb8ea7dcd58b02029a9b2031fe0998a89d
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-06-27 18:18:31 +0900

    add new command(enabled_repetition_host_in_routingdump)

--- ruby/server/lib/roma/plugin/plugin_gui.rb
+++ ruby/server/lib/roma/plugin/plugin_gui.rb
@@ -1,3 +1,4 @@
+require 'json'
 
 module Roma
   module CommandPlugin
@@ -19,6 +20,26 @@ module Roma
         send_data("END\r\n")
       end
 
+      def ev_enabled_repetition_host_in_routingdump(s)
+        rcv = send_cmd(@stats.ap_str, "routingdump json\r\n")
+        hash = JSON.parse(rcv)
+        repetition = false
+        hash[2].each_value{|value|
+          host = []
+          value.map{|instance|
+            host << instance.split("_")[0]
+          }
+          if host.uniq!
+            repetition = true
+            break
+          end
+        }
+        if repetition
+          send_data("true\r\n")
+        else
+          send_data("false\r\n")
+        end
+      end
     end
   end
 end
