a00e56823a88966004d0e08bd56eea121c339b0c
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-07-07 17:01:29 +0900

    add [get_logs] function to plugin_gui.rb

--- ruby/server/lib/roma/plugin/plugin_gui.rb
+++ ruby/server/lib/roma/plugin/plugin_gui.rb
@@ -59,6 +59,36 @@ module Roma
         send_data("END\r\n")
       end
 
+      # get_logs [line count]
+      def ev_get_logs(s)
+        if s.length != 2
+          return send_data("CLIENT_ERROR number of arguments (#{s.length-1} for 1)\r\n")
+        end
+
+        get_line_count = s[1].to_i
+        log_path = get_config_stat["config.LOG_PATH"]
+        log_file = "#{log_path}/#{@stats.ap_str}.log"
+
+        raw_logs = []
+        f = File.new(log_file)
+        f.each_line{|line|
+          raw_logs << line
+        }
+
+        sliced_logs = []
+        if raw_logs.size > get_line_count
+          sliced_logs = raw_logs.slice(-get_line_count..-1)
+        else
+          sliced_logs = raw_logs
+        end
+
+        sliced_logs.each{|line|
+          send_data(line)
+        }
+
+        send_data("END\r\n")
+      end
+
     end
   end
 end
