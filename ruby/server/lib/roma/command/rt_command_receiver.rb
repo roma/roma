f36e9076c82f426d99ebe31e8512653970fd5986
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2014-09-03 02:50:47 +0000

    add live_patch-20100330-003.rb to ROMA

--- ruby/server/lib/roma/command/rt_command_receiver.rb
+++ ruby/server/lib/roma/command/rt_command_receiver.rb
@@ -288,6 +288,23 @@ module Roma
         end
       end
 
+      # get_key_info <key>
+      def ev_get_key_info(s)
+        if s.length != 2
+          return send_data("CLIENT_ERROR number of arguments(0 for 1)\r\n")
+        end
+
+        d = Digest::SHA1.hexdigest(s[1]).hex % @rttable.hbits
+        vn = @rttable.get_vnode_id(d)
+        nodes = @rttable.search_nodes_for_write(vn)
+        s = sprintf("d = %s 0x%x\r\n",d,d)
+        send_data(s)
+        s = sprintf("vn = %s 0x%x\r\n",vn,vn)
+        send_data(s)
+        send_data("nodes = #{nodes.inspect}\r\n")
+        send_data("END\r\n")
+      end
+
     end # module RoutingCommandReceiver
   end # module Command
 end # module Roma
