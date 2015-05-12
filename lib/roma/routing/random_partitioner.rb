74e7ee58ff00505c7b2137980548239e5fce39f0
Hiroki Matsue <hiroki.matsue@gmail.com>
2015-05-12 17:23:13 +0900

    Fix random routing logic for join

--- lib/roma/routing/random_partitioner.rb
+++ lib/roma/routing/random_partitioner.rb
@@ -1,24 +1,23 @@
 module Roma
   module Routing
     module RandomPartitioner
-
       def exclude_nodes(ap_str, rep_host)
-        exclude_nodes = self.nodes
+        exclude_nodes = nodes
         if rep_host
           exclude_nodes = [ap_str]
         else
           myhost = ap_str.split(/[:_]/)[0]
-          exclude_nodes.delete_if{|nid| nid.split(/[:_]/)[0] != myhost }
+          exclude_nodes.delete_if { |nid| nid.split(/[:_]/)[0] != myhost }
         end
         exclude_nodes
       end
 
-      def exclude_nodes_for_join(ap_str, rep_host)
+      def exclude_nodes_for_join(ap_str, _rep_host)
         [ap_str]
       end
 
-      alias :exclude_nodes_for_recover :exclude_nodes
-      alias :exclude_nodes_for_balance :exclude_nodes
+      alias_method :exclude_nodes_for_recover, :exclude_nodes
+      alias_method :exclude_nodes_for_balance, :exclude_nodes
 
       def myhost_include?(nodes, myhost)
         nodes.each do |nid|
@@ -29,7 +28,7 @@ module Roma
       private :myhost_include?
 
       # vnode sampling exclude +exclude_nodes+
-      def select_vn_for_join(exclude_nodes)
+      def select_vn_for_join(exclude_nodes, rep_host)
         short_idx = {}
         myhost_idx = {}
         idx = {}
@@ -61,15 +60,22 @@ module Roma
         ks = idx.keys
         vn = ks[rand(ks.length)]
         nids = idx[vn]
-        [vn, nids, rand(@rd.rn) == 0]
+
+        if !rep_host && nids[0].split(/[:_]/)[0] == myhost
+          is_primary = true
+        else
+          is_primary = rand(@rd.rn) == 0
+        end
+
+        [vn, nids, is_primary]
       end
 
       # select a vnodes where short of redundancy.
-      def select_vn_for_recover(exclude_nodes)
+      def select_vn_for_recover(exclude_nodes, _rep_host)
         ret = []
         @rd.v_idx.each_pair do |vn, nids|
-          if nids.length < @rd.rn && list_include?(nids,exclude_nodes) == false
-            ret << [vn,nids]
+          if nids.length < @rd.rn && list_include?(nids, exclude_nodes) == false
+            ret << [vn, nids]
           end
         end
         if ret.length == 0
@@ -81,27 +87,27 @@ module Roma
       end
 
       def select_node_for_release(ap_str, rep_host, nids)
-        buf = self.nodes
+        buf = nodes
 
         unless rep_host
           deny_hosts = []
-          nids.each{ |nid|
+          nids.each do |nid|
             host = nid.split(/[:_]/)[0]
             deny_hosts << host if host != ap_str.split(/[:_]/)[0]
-          }
-          buf.delete_if{|nid| deny_hosts.include?(nid.split(/[:_]/)[0])}
+          end
+          buf.delete_if { |nid| deny_hosts.include?(nid.split(/[:_]/)[0]) }
         else
-          nids.each{|nid| buf.delete(nid) }
+          nids.each { |nid| buf.delete(nid) }
         end
 
-        buf.delete_if{|instance| instance == ap_str}
+        buf.delete_if { |instance| instance == ap_str }
         to_nid = buf.sample
-        new_nids = nids.map{|n| n == ap_str ? to_nid : n }
+        new_nids = nids.map { |n| n == ap_str ? to_nid : n }
         [to_nid, new_nids]
       end
 
       # vnode sampling exclude +exclude_nodes+
-      def select_vn_for_balance(exclude_nodes)
+      def select_vn_for_balance(exclude_nodes, _rep_host)
         short_idx = {}
         idx = {}
         @rd.v_idx.each_pair do |vn, nids|
@@ -118,7 +124,6 @@ module Roma
         nids = idx[vn]
         [vn, nids, rand(@rd.rn) == 0]
       end
-
     end
   end
 end
