d51763fe41ce57d6a5c5748bf540327a662ee4e7
Paras Patel <parasprince2001@gmail.com>
2015-05-12 11:47:31 +0900

    Removed unnecessary comments from log_shift test script

--- test/t_logshift.rb
+++ test/t_logshift.rb
@@ -21,7 +21,7 @@ class  LogShiftTest  < Test::Unit::TestCase
   end
 
   def test_log_shift_size
-    # get Current log_shift_size and log_shift_age of port 11211
+    # get Current log_shift_size of port 11211
     @sock_port_1.write("stat log_shift_size\r\n")
     old_log_shift_size = @sock_port_1.gets.chomp
 
@@ -42,11 +42,11 @@ class  LogShiftTest  < Test::Unit::TestCase
   end
 
     def test_log_shift_age
-    # get Current log_shift_size and log_shift_age of port 11211
+    # get Current log_shift_age of port 11211
     @sock_port_1.write("stat log_shift_age\r\n")
     old_log_shift_age = @sock_port_1.gets.chomp
 
-    # set & check new log_shift_size value on port 11211
+    # set & check new log_shift_age value on port 11211
     @sock_port_1.write("set_log_shift_age 7\r\n")
     assert_equal("END", @sock_port_1.gets.chomp)
     assert_equal('{"localhost_11212"=>"STORED", "localhost_11211"=>"STORED"}', @sock_port_1.gets.chomp)
@@ -55,7 +55,7 @@ class  LogShiftTest  < Test::Unit::TestCase
     assert_equal( "stats.log_shift_age 7" , @sock_port_1.gets.chomp);
     assert_equal("END", @sock_port_1.gets.chomp)
 
-    # get & check new log_shift_size value on port 11212
+    # get & check new log_shift_age value on port 11212
     @sock_port_2.write("stat log_shift_age\r\n")
     assert_equal( "stats.log_shift_age 7" , @sock_port_2.gets.chomp);
     assert_equal("END", @sock_port_2.gets.chomp)
@@ -63,11 +63,11 @@ class  LogShiftTest  < Test::Unit::TestCase
   end
 
   def test_log_shift_age_min
-    # get Current log_shift_size and log_shift_age of port 11211
+    # get Current log_shift_age of port 11211
     @sock_port_1.write("stat log_shift_age\r\n")
     old_log_shift_age = @sock_port_1.gets.chomp
 
-    # set & check new log_shift_size value on port 11211
+    # set & check new log_shift_age value on port 11211
     @sock_port_1.write("set_log_shift_age min\r\n")
     assert_equal("END", @sock_port_1.gets.chomp)
     assert_equal('{"localhost_11212"=>"STORED", "localhost_11211"=>"STORED"}', @sock_port_1.gets.chomp)
@@ -76,7 +76,7 @@ class  LogShiftTest  < Test::Unit::TestCase
     assert_equal( "stats.log_shift_age min" , @sock_port_1.gets.chomp);
     assert_equal("END", @sock_port_1.gets.chomp)
 
-    # get & check new log_shift_size value on port 11212
+    # get & check new log_shift_age value on port 11212
     @sock_port_2.write("stat log_shift_age\r\n")
     assert_equal( "stats.log_shift_age min" , @sock_port_2.gets.chomp);
     assert_equal("END", @sock_port_2.gets.chomp)
