5fbfab5f2eda093134dc1ad7468d1b8f75f09a7d
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2015-06-22 15:28:14 +0900

    start dev

---
+++ lib/roma/tools/adm.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+module Roma
+  class Adm
+    def initialize(cmd)
+      @cmd = cmd
+    end
+
+    #def check_type
+    #  case @cmd
+    #  when "detach"
+    #    @check = true
+    #  when "start", "recover-node", "dump-ring"
+    #    @alias = true
+    #  else
+    #    @alias = false # no need?
+    #    @check = false # no need?
+    #  end
+    #end
+
+    def make_command
+      #case @cmd
+      #when "detach"
+      #  @cmd = "shutdown-self\r\nyes\r\n"
+      #when "start"
+      #  
+      #when "join"
+      #when "snapshot"
+      #when "status"
+      #else
+        
+      #end
+    end
+
+    def send_command(node="localhost_10001")
+      #if @alias
+      #  `#{@cmd}`
+      #elsif @check
+      #else
+        puts `echo #{@cmd} | nc -i1 #{node.split("_")[0]} #{node.split("_")[1]}`
+      #end
+    end
+  end #Adm
+end # Roma
+
+if ARGV.length < 1
+  puts File.basename(__FILE__) + " <adm-command> [node]"
+  exit
+end
+
+#sc = Roma::SafeCopy.new("localhost", ARGV[0].to_i)
+adm = Roma::Adm.new(ARGV[0])
+
+begin
+  #adm.check_type
+  #adm.make_command
+  adm.send_command
+rescue
+  puts "Unexpected Error"
+ensure
+end
