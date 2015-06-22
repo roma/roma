19e31f36a4630578859e026fc46c2caccd8c4f61
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2015-06-22 18:51:41 +0900

    enable to execute alias command like a romad

--- lib/roma/tools/adm.rb
+++ lib/roma/tools/adm.rb
@@ -10,20 +10,29 @@ module Roma
 
     def check_type
       case @cmd
-      when "balse", "shutdown_self"
+      when "balse", "shutdown_self" # yes/no check
         puts("Are you sure?(yes/no)\r\n")
         if STDIN.gets.chomp != "yes"
           raise "confirmation was rejected"
         else
           @cmd.concat("\r\nyes\r\nyes\r\n")
         end
+      when "start" # alias cmd
+        puts("Please input hostname or ip address which is used for ROMA.\r\n")
+        hostname = STDIN.gets.chomp
+        puts("Please input port No. which is used for ROMA.\r\n")
+        port = STDIN.gets.chomp
+        puts("Please input PATH of config.rb.\r\n")
+        config_path = STDIN.gets.chomp
+        @cmd = "bin/romad #{hostname} -p #{port} -d --config #{config_path}"
+        @alias = true
       else
     #  when "detach"
     #    @check = true
     #  when "start", "recover-node", "dump-ring"
     #    @alias = true
     #  else
-    #    @alias = false # no need?
+        @alias = false # no need?
     #    @check = false # no need?
       end
     end
@@ -43,12 +52,13 @@ module Roma
     end
 
     def send_command(node="localhost_10001")
-      #if @alias
-      #  `#{@cmd}`
+      if @alias
+        base_path = Pathname(__FILE__).dirname.parent.parent.parent.expand_path
+        `#{base_path}/#{@cmd}`
       #elsif @check
-      #else
+      else
         puts `echo -e "#{@cmd}" | nc -i1 #{node.split("_")[0]} #{node.split("_")[1]}`
-      #end
+      end
     end
   end #Adm
 end # Roma
