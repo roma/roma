3a21ade0f346cb80a90c4edf2a63740bc49fed17
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2015-07-14 14:53:10 +0900

    undergoing

--- test/t_routing_logic.rb
+++ test/t_routing_logic.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
+require '/usr/local/rvm/gems/ruby-2.1.2/gems/test-unit-3.1.2/lib/test/unit/data' # todo
+require '/usr/local/rvm/gems/ruby-2.1.2/gems/test-unit-3.1.2/lib/test/unit/attribute' # todo
+
 class RoutingLogicTest < Test::Unit::TestCase
   include RomaTestUtils
+  include Test::Unit::Data
+  include Test::Unit::Attribute
 
   NEW_PORTS = %w(11213 11214)
 
@@ -11,17 +16,15 @@ class RoutingLogicTest < Test::Unit::TestCase
     puts "#{e} #{$ERROR_POSITION}"
   end
 
-  data = {
-  'single_host' => [
-    true,
-    NEW_PORTS.map { |port| "#{DEFAULT_HOST}_#{port}" }],
-  'multiple_hosts' => [
-    false,
-    NEW_PORTS.map { |port| "#{DEFAULT_IP}_#{port}" }],
-  'multiple_hosts_include_some_hosts' => [
-    false,
-    ["#{DEFAULT_IP}_#{NEW_PORTS[0]}", "#{DEFAULT_HOST}_#{NEW_PORTS[1]}"]]
-  }
+  a = data(
+       'single_host' => [true, NEW_PORTS.map { |port| "#{DEFAULT_HOST}_#{port}" }],
+       #'multiple_hosts' => [false, NEW_PORTS.map { |port| "#{DEFAULT_IP}_#{port}" }],
+       #'multiple_hosts_include_some_hosts' => [false, ["#{DEFAULT_IP}_#{NEW_PORTS[0]}", "#{DEFAULT_HOST}_#{NEW_PORTS[1]}"]]
+  )
+
+  puts "asdfadsf"
+  puts a
+  puts "asdfadsf"
 
   def test_join(data)
     replication_in_host, new_nodes = data
@@ -29,7 +32,7 @@ class RoutingLogicTest < Test::Unit::TestCase
     sleep 12 # Wait cluster starting, otherwise join will never finish
     client = get_client
 
-    # Join nodes
+   # Join nodes
     assert_equal(DEFAULT_NODES.size, client.rttable.nodes.size)
     new_nodes.each do |new_node|
       join_roma(new_node, replication_in_host: replication_in_host)
@@ -70,52 +73,52 @@ class RoutingLogicTest < Test::Unit::TestCase
     stats = client.stats(node: new_node)
     assert_match(/#{new_node}/, stats['routing.nodes'])
     assert_not_equal(0, stats['routing.secondary'].to_i)
-  end
-
-  def test_join_when_num_of_hosts_changing
-    replication_in_host = false
-    same_host_node = "#{DEFAULT_HOST}_#{NEW_PORTS[0]}"
-    other_host_node = "#{DEFAULT_IP}_#{NEW_PORTS[1]}"
-
-    start_roma(div_bits: 6, replication_in_host: replication_in_host)
-    sleep 12 # Wait cluster starting, otherwise join will never finish
-    client = get_client
-
-    # Join other_host_node
-    join_roma(other_host_node, replication_in_host: replication_in_host)
-    wait_join(other_host_node)
-    stats = client.stats(node: other_host_node)
-    assert_match(/#{other_host_node}/, stats['routing.nodes'])
-    assert_not_equal(0, stats['routing.secondary'].to_i)
-
-    # Stop other_host_node and generate short vnodes
-    stop_roma_node(other_host_node)
-    wait_failover(other_host_node)
-    stats = client.stats
-    assert_no_match(/#{other_host_node}/, stats['routing.nodes'])
-    assert_not_equal(0, stats['routing.short_vnodes'].to_i)
-
-    # Join other_host_node
-    join_roma(other_host_node, replication_in_host: replication_in_host)
-    wait_join(other_host_node)
-    stats = client.stats(node: other_host_node)
-    assert_match(/#{other_host_node}/, stats['routing.nodes'])
-    assert_not_equal(0, stats['routing.secondary'].to_i)
-
-    # Stop other_host_node and generate short vnodes
-    stop_roma_node(other_host_node)
-    wait_failover(other_host_node)
-    stats = client.stats
-    assert_no_match(/#{other_host_node}/, stats['routing.nodes'])
-    assert_not_equal(0, stats['routing.short_vnodes'].to_i)
-
-    # Join same_host_node (secondary will be 0 in this case)
-    join_roma(same_host_node, replication_in_host: replication_in_host)
-    wait_join(same_host_node)
-    stats = client.stats(node: same_host_node)
-    assert_match(/#{same_host_node}/, stats['routing.nodes'])
-    assert_equal(0, stats['routing.secondary'].to_i)
-  end
+   end
+#
+#  def test_join_when_num_of_hosts_changing
+#    replication_in_host = false
+#    same_host_node = "#{DEFAULT_HOST}_#{NEW_PORTS[0]}"
+#    other_host_node = "#{DEFAULT_IP}_#{NEW_PORTS[1]}"
+#
+#    start_roma(div_bits: 6, replication_in_host: replication_in_host)
+#    sleep 12 # Wait cluster starting, otherwise join will never finish
+#    client = get_client
+#
+#    # Join other_host_node
+#    join_roma(other_host_node, replication_in_host: replication_in_host)
+#    wait_join(other_host_node)
+#    stats = client.stats(node: other_host_node)
+#    assert_match(/#{other_host_node}/, stats['routing.nodes'])
+#    assert_not_equal(0, stats['routing.secondary'].to_i)
+#
+#    # Stop other_host_node and generate short vnodes
+#    stop_roma_node(other_host_node)
+#    wait_failover(other_host_node)
+#    stats = client.stats
+#    assert_no_match(/#{other_host_node}/, stats['routing.nodes'])
+#    assert_not_equal(0, stats['routing.short_vnodes'].to_i)
+#
+#    # Join other_host_node
+#    join_roma(other_host_node, replication_in_host: replication_in_host)
+#    wait_join(other_host_node)
+#    stats = client.stats(node: other_host_node)
+#    assert_match(/#{other_host_node}/, stats['routing.nodes'])
+#    assert_not_equal(0, stats['routing.secondary'].to_i)
+#
+#    # Stop other_host_node and generate short vnodes
+#    stop_roma_node(other_host_node)
+#    wait_failover(other_host_node)
+#    stats = client.stats
+#    assert_no_match(/#{other_host_node}/, stats['routing.nodes'])
+#    assert_not_equal(0, stats['routing.short_vnodes'].to_i)
+#
+#    # Join same_host_node (secondary will be 0 in this case)
+#    join_roma(same_host_node, replication_in_host: replication_in_host)
+#    wait_join(same_host_node)
+#    stats = client.stats(node: same_host_node)
+#    assert_match(/#{same_host_node}/, stats['routing.nodes'])
+#    assert_equal(0, stats['routing.secondary'].to_i)
+#  end
 
   # TODO: recover, balance command tests
 end
